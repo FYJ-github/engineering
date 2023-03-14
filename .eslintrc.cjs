b9d5104395555fe789e2bed69b66e90835dd4532
FYJ-github <fyj18780754826@126.com>
2023-03-14 17:18:03 +0800

    feat: 安装Jest进行单元测试

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,18 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: ['standard', 'eslint-config-standard-with-typescript', 'prettier'],
   overrides: [],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json'
   },
   plugins: ['@typescript-eslint', 'prettier'],
-    rules: {
-      'prettier/prettier': 'error'
-  },
-};
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
--- src/indes.ts
+++ src/indes.ts
@@ -2,3 +2,5 @@ const calc = (a: number, b: number): number => {
   return a - b
 }
 console.log(calc(1024, 28))
+
+export default calc
